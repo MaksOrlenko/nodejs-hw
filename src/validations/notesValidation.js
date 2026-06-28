ad96e03c74458db1ac1531bbbbc3e618ed3c46e8
MaksOrlenko <144942642+MaksOrlenko@users.noreply.github.com>
2026-06-28 13:02:54 +0300

    Fix valid

--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -1,36 +1,57 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import { errors } from 'celebrate'; 
-import { connectMongoDB } from './db/connectMongoDB.js';
-import { logger } from './middleware/logger.js';
-import { notFoundHandler } from './middleware/notFoundHandler.js';
-import { errorHandler } from './middleware/errorHandler.js';
-import notesRouter from './routes/notesRoutes.js';
+import { Joi, Segments } from 'celebrate';
+import { isValidObjectId } from 'mongoose';
+import { TAGS } from '../constants/tags.js';
 
-dotenv.config();
-
-const app = express();
-
-app.use(logger);
-app.use(cors());
-app.use(express.json());
-
-app.use(notesRouter);
-
-app.use(errors());
-
-app.use(notFoundHandler);
-app.use(errorHandler);
-
-const PORT = process.env.PORT || 3000;
-
-export const startServer = async () => {
-  await connectMongoDB();
-
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
+const validateObjectId = (value, helpers) => {
+  if (!isValidObjectId(value)) {
+    return helpers.message('Invalid MongoDB ObjectId');
+  }
+  return value;
 };
 
-startServer();
+export const getAllNotesSchema = {
+  [Segments.QUERY]: Joi.object().keys({
+    page: Joi.number().integer().min(1).default(1),
+    perPage: Joi.number().integer().min(5).max(20).default(10),
+    tag: Joi.string()
+      .valid(...TAGS)
+      .optional(),
+    search: Joi.string().allow('').optional(),
+  }),
+};
+
+export const noteIdSchema = {
+  [Segments.PARAMS]: Joi.object().keys({
+    noteId: Joi.string()
+      .custom(validateObjectId, 'ObjectID validation')
+      .required(),
+  }),
+};
+
+// 3. Схема для POST /notes (Валідація тіла запиту при створенні)
+export const createNoteSchema = {
+  [Segments.BODY]: Joi.object().keys({
+    title: Joi.string().min(1).required(),
+    content: Joi.string().allow('').optional(),
+    tag: Joi.string()
+      .valid(...TAGS)
+      .optional(),
+  }),
+};
+
+export const updateNoteSchema = {
+  [Segments.PARAMS]: Joi.object().keys({
+    noteId: Joi.string()
+      .custom(validateObjectId, 'ObjectID validation')
+      .required(),
+  }),
+  [Segments.BODY]: Joi.object()
+    .keys({
+      title: Joi.string().min(1).optional(),
+      content: Joi.string().allow('').optional(),
+      tag: Joi.string()
+        .valid(...TAGS)
+        .optional(),
+    })
+    .or('title', 'content', 'tag'),
+};
